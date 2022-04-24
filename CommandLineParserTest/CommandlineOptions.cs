e367ee34f55f495e05bddbf9998333be388f31b7
9kb <51182838+9kbx@users.noreply.github.com>
2022-04-24 10:35:36 +0800

    init

---
+++ CommandLineParserTest/CommandlineOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParserTest
+{
+    public class CommandlineOptions
+    {
+        [Option('h', "host", Default = "localhost", HelpText = "Hostname on to listen")]
+        public String Host { get; set; }
+
+        [Option('p', "port", Default = 9876, HelpText = "Port HTTP proxy will be listening to", Required = false)]
+        public Int32 HttpPort { get; set; }
+    }
+}
---
+++ CommandLineParserTest/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
+using CommandLine.Text;
+using CommandLineParserTest;
+
+Console.WriteLine("Hello, World!");
+
+
+var parserResult = Parser.Default.ParseArguments<CommandlineOptions>(args);
+parserResult.WithParsed<CommandlineOptions>(options => RunTest(options));
+parserResult.WithNotParsed<CommandlineOptions>(errs =>
+{
+    var helpText = HelpText.AutoBuild(parserResult, h =>
+    {
+        // Configure HelpText here  or create your own and return it 
+        h.AdditionalNewLineAfterOption = false;
+        return HelpText.DefaultParsingErrorsHandler(parserResult, h);
+    }, e =>
+    {
+        return e;
+    });
+    Console.Error.Write(helpText);
+});
+
+static void RunTest(CommandlineOptions options)
+{
+    Console.WriteLine($"host = {options.Host}");
+    Console.WriteLine($"port = {options.HttpPort}");
+
+    // var service = new Service();
+    //Task.WhenAll(new[]
+    //            {
+    //                //service1.Start(),
+    //                //service2.Start()
+    //            });
+
+    while (Console.ReadLine() != "exit")
+    {
+        Thread.Sleep(TimeSpan.FromSeconds(1));
+    }
+}
